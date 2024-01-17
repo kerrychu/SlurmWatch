ac048882d8afd90518a623a28b47a5dacb52368a
kerrychu <kerry.x.chu@gmail.com>
2024-01-17 14:16:35 +1000

    add basic logs for debugging

--- hooks/slack.py
+++ hooks/slack.py
@@ -3,8 +3,11 @@ import json
 from typing import Dict
 
 
-def send_slack_message(data: Dict[str, str], url: str):
+def send_slack_message(data: Dict[str, str], url: str) -> str:
     payload = json.dumps(data)
     headers = {"Content-type": "application/json"}
-    response = requests.request("POST", url, headers=headers, data=payload)
-    return response
+    response = requests.request(
+        "POST", url, headers=headers, data=payload, timeout=3600
+    )
+    print({"response": response.text, "response_code": response.status_code})
+    return response.text
