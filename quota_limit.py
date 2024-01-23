79dd0684d032b5b30a2a3e51a5d267efb9ab9fd4
kerrychu <kerry.x.chu@gmail.com>
2024-01-23 11:46:06 +1000

    adding usage percentage to quota

--- quota_limit.py
+++ quota_limit.py
@@ -21,10 +21,10 @@ def monitor():
     for project_id in PROJECT_IDs:
         raw_quota = get_fileset_quota(project_id)
         quota_record = stdout_to_quota_records(raw_quota)
-        slack_message = "💿 Current State of Quota\n"
+        usage = round(int(quota_record[ "Used Storage (GB)"]) / int(quota_record["Storage Limit (GB)"]), 2)
+        slack_message = "💿 Current State of Quota\n" + f"usage percentage: {usage * 100}%"
         for key, value in quota_record.items():
             slack_message += f"⦿ {key}: {value}\n"
-
         send_slack_message(message=slack_message, webhook=SLACK_WEBHOOK)
 
 
